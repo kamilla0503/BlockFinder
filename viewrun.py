5870642a12006126163fd2be523e1d75ec809c6d
Maxim A. Dubinnyi <dumkas@yandex.ru>
2019-09-26 12:48:22 +0300

    viewrun.py status report

---
+++ viewrun.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python -u
+
+import argparse
+import re
+
+def scan_tasks(taskfile):
+   num_tasks = 0
+   tasks = []
+   with open(taskfile, "r") as task:
+      for line in task:
+         num_tasks+= 1
+   return(num_tasks)
+
+task_run_re= re.compile('^\\[Task(\d+)\\] +(\d+)M +(\d+) sec')
+task_finished_re = re.compile('^\\[Task(\d+)\\] finished task (\d+) after (\d+) iterations')
+
+def scan_log(logfiles_list, n_tasks):
+   log_lines_in_all_files = 0
+   list_task_time = [None for t in range(n_tasks)]
+   list_task_iter = [None for t in range(n_tasks)]
+   list_task_finished = [False for t in range(n_tasks)]
+
+   for logfile in logfiles_list:
+      print("Start {} scan".format(logfile))
+      with open(logfile, "r") as log:
+        log_lines = 0
+        for line in log:
+           log_lines += 1
+           log_lines_in_all_files += 1
+           task_run_match = task_run_re.match(line)
+           if task_run_match:
+              task_num =  int(task_run_match.group(1))
+              task_iter = int(task_run_match.group(2))*1000000
+              task_sec  = int(task_run_match.group(3))
+              list_task_iter[task_num] = task_iter
+              list_task_time[task_num] = task_sec
+              continue
+           task_finished_match = task_finished_re.match(line)
+           if task_finished_match:
+              task_num =  int(task_finished_match.group(1))
+              task_iter = int(task_finished_match.group(3))
+              list_task_iter[task_num] = task_iter
+              list_task_finished[task_num] = True
+              
+        print("Scan of log file {} finished, {:,} lines scanned".format(logfile, log_lines))
+        
+   print("Scan of all {} log files finished, {:,} lines scanned".\
+      format(len(logfiles_list), log_lines_in_all_files))
+
+   total_iter = 0
+   total_finished = 0
+   total_none = 0
+   total_running = 0
+   for task_num in range(n_tasks):
+      if list_task_iter[task_num]:
+         total_iter += list_task_iter[task_num]
+         if not list_task_finished[task_num]:
+            total_running += 1
+      else:
+         total_none += 1
+      if list_task_finished[task_num]:
+         total_finished += 1
+    
+   print("Task status: {} finished, {} running and {} waiting ".format(total_finished, total_running, total_none))
+   print("Total {:,} iterations in all tasks".format(total_iter))
+
+
+   return( (list_task_time, list_task_iter, list_task_finished) )
+         
+      
+
+if __name__ == '__main__':
+   parser = argparse.ArgumentParser()
+   parser.add_argument("log", help='Log file to scan', type=str, nargs='+')
+   parser.add_argument("--tasks", "-t", help='Tasks file', type=str, default="tasksend.txt")
+   args = parser.parse_args()
+   n_tasks = scan_tasks(args.tasks)
+   print("Number of tasks = {}".format(n_tasks))
+   stat = scan_log(args.log, n_tasks)
