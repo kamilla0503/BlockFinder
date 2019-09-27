1be26af117ae4cf2afe697a6b08100ac3943db55
Maxim A. Dubinnyi <dumkas@yandex.ru>
2019-09-27 14:50:45 +0300

    viewrun.py prints status of all tasks

--- viewrun.py
+++ viewrun.py
@@ -2,6 +2,7 @@
 
 import argparse
 import re
+import math
 
 def scan_tasks(taskfile):
    num_tasks = 0
@@ -51,18 +52,42 @@ def scan_log(logfiles_list, n_tasks):
    total_finished = 0
    total_none = 0
    total_running = 0
+
+   out_line_len = 100
+   out_n_lines = math.floor(n_tasks/out_line_len) + 1
+   out_line = "{:>5} ".format(1)
+   out_line_num = 0
+   
    for task_num in range(n_tasks):
+      
+      if not ( task_num % out_line_len ) and not task_num == 0:
+         out_line += " {:>5}".format(task_num)
+         print(out_line)
+         out_line_num += 1
+         out_line = ""
+         out_line += "{:>5} ".format(task_num+1)
+
+
       if list_task_iter[task_num]:
          total_iter += list_task_iter[task_num]
          if not list_task_finished[task_num]:
             total_running += 1
+            out_line += "*"
       else:
          total_none += 1
+         out_line += "."
       if list_task_finished[task_num]:
          total_finished += 1
+         out_line += "F"
+   
+   rest_size = ( n_tasks ) % out_line_len
+   out_line_fill= out_line + (" " * (out_line_len - rest_size ) )+ " {:>5}".format(n_tasks)
+   print(out_line_fill)
+      
     
    print("Task status: {} finished, {} running and {} waiting ".format(total_finished, total_running, total_none))
    print("Total {:,} iterations in all tasks".format(total_iter))
+
 
 
    return( (list_task_time, list_task_iter, list_task_finished) )
