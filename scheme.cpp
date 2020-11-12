0d3c2c7c5be07c577a16feff520b974f3d5c1faa
Maxim A. Dubinnyi <dumkas@yandex.ru>
2020-11-12 11:47:51 +0300

    nolde_speed merged 2

--- scheme.cpp
+++ scheme.cpp
@@ -26,96 +26,96 @@ bool operator==(const Scheme_compact& t1, const Scheme_compact& s2) {
 
 
 Scheme::Scheme() {
-	patterns={};
+    patterns={};
 }
 
 
 bool Scheme::check_codes() {
-	int code;
-	bool first = true; 
-	codes = false;
+    int code;
+    bool first = true; 
+    codes = false;
 
     //cout << " check codes[1]" << codes[1] << endl;
     //codes= codes.shift(false);
-	for (int i: patterns) {
-		for ( int j : patterns) {
-			//code =  patternscode.codes[ patternscode.patterns.size()*i  +j]; //!!!
-			code =  code_tab_ptr->calc_code_fast(i, j);
-			if (first) {
-				first = false;
-				continue;
-			}
-			else if (codes[code]==true) {
-				return false;
-			}
-			else {
-				//codes.insert(code);
-				codes[code] = true;
-			}
-		}
-	}
-	return true;
+    for (int i: patterns) {
+        for ( int j : patterns) {
+            //code =  patternscode.codes[ patternscode.patterns.size()*i  +j]; //!!!
+            code =  code_tab_ptr->calc_code_fast(i, j);
+            if (first) {
+                first = false;
+                continue;
+            }
+            else if (codes[code]==true) {
+                return false;
+            }
+            else {
+                //codes.insert(code);
+                codes[code] = true;
+            }
+        }
+    }
+    return true;
 }
 
 
 void Scheme::setscheme( PatternsCodes *patternscode , string sname, NCS *sncs, int  bsamples, vector <int>  bpatterns) {
-	name = sname;
+    name = sname;
     patterns=bpatterns;
-	samples = bsamples;
-	code_tab_ptr = patternscode;
-	ncs_ptr = sncs;
-	codes.resize(code_tab_ptr->n_codes, false);
+    samples = bsamples;
+    code_tab_ptr = patternscode;
+    ncs_ptr = sncs;
+    codes.resize(code_tab_ptr->n_codes, false);
 
-	good = check_codes();
-	simplify();
+    good = check_codes();
+    simplify();
 
 }
 
 
 void Scheme::simplify() {
-	simplified.assign(code_tab_ptr->n_simplified, 0);
-	for (int pattern : patterns) {
-	   simplified[code_tab_ptr->simple_ints[pattern]]++;
-	}
+    simplified.assign(code_tab_ptr->n_simplified, 0);
+    for (int pattern : patterns) {
+       simplified[code_tab_ptr->simple_ints[pattern]]++;
+    }
 }
 
 
 Scheme::Scheme(PatternsCodes *patternscode, string sname, NCS *sncs, int  bsamples, vector <int>  bpatterns) {
-	name = sname;
+    name = sname;
     patterns = bpatterns;
-	samples = bsamples;
-	code_tab_ptr = patternscode; 
-	ncs_ptr  = sncs;
-	Vbool codes(false, code_tab_ptr->n_codes);
-	good = check_codes();
-	simplify();
+    samples = bsamples;
+    code_tab_ptr = patternscode; 
+    ncs_ptr  = sncs;
+    Vbool codes(false, code_tab_ptr->n_codes);
+    good = check_codes();
+    simplify();
 
 }
 
 
 bool Scheme::check_patterns(vector <string> patterns) {
-	if (patterns.size() == 0) {
-		return false;
-	}
-	int sizep;
-	sizep = patterns[0].size();
-	for (string pattern : patterns) {
-		for (char label_type : pattern) {
-			if (find(ncs_ptr->label_types.begin(), ncs_ptr->label_types.end(), to_string(label_type)) == ncs_ptr->label_types.end()) { //-/ 
-				return false;
-			}
-		}
-		if (pattern.size() != sizep) {
-			return false;
-		}
-	}
-	return true;
+    if (patterns.size() == 0) {
+        return false;
+    }
+    int sizep;
+    sizep = patterns[0].size();
+    for (string pattern : patterns) {
+        for (char label_type : pattern) {
+            if (find(ncs_ptr->label_types.begin(), ncs_ptr->label_types.end(), to_string(label_type)) == ncs_ptr->label_types.end()) { //-/ 
+                return false;
+            }
+        }
+        if (pattern.size() != sizep) {
+            return false;
+        }
+    }
+    return true;
 }
 
 
 void Scheme::sort(){
 
-	std::sort(patterns.begin(), patterns.end());
+    std::sort(patterns.begin(), patterns.end());
 
 }
 
@@ -128,41 +128,41 @@ void Scheme_compact::sort(){
 
 
 void Scheme::add_new_codes(int new_pattern) {
-	int m=code_tab_ptr->patterns.size();
-	int n=new_pattern;
+    int m=code_tab_ptr->patterns.size();
+    int n=new_pattern;
     int j=0;
-	for (int i :patterns) {
+    for (int i :patterns) {
         codes[code_tab_ptr->calc_code_fast(i,n)]=true;
         codes[code_tab_ptr->calc_code_fast(n,i)]=true;
-	}
+    }
     codes[code_tab_ptr->calc_code_fast(n,n)]=true;
 }
 
 
 void Scheme::add_pattern(int new_pattern) {
-	patterns.push_back(new_pattern);
-	add_new_codes(new_pattern);
-	simplify();
+    patterns.push_back(new_pattern);
+    add_new_codes(new_pattern);
+    simplify();
 }
 
 
 bool Scheme::try_pattern(int  new_pattern) {
-	if (good == false) {
-		return false;
-	}
-	
+    if (good == false) {
+        return false;
+    }
+    
     int i_code[2*patterns.size()+1];
-	 
-   	int n = new_pattern;
+     
+    int n = new_pattern;
 
-	for (int i=0; i<patterns.size(); i++) {
+    for (int i=0; i<patterns.size(); i++) {
         int i_code1 = code_tab_ptr->calc_code_fast(patterns[i],n);
         if(codes[i_code1]) return false;
         int i_code2 = code_tab_ptr->calc_code_fast(n,patterns[i]);
         if(codes[i_code2]) return false;
-		i_code[2*i] = i_code1;
-		i_code[2*i+1] = i_code2;
-	}
+        i_code[2*i] = i_code1;
+        i_code[2*i+1] = i_code2;
+    }
     int i_code0 = code_tab_ptr->calc_code_fast(n,n);
     if(codes[i_code0]) return false;
     i_code[2*patterns.size()] = i_code0;
@@ -173,19 +173,19 @@ bool Scheme::try_pattern(int  new_pattern) {
         else new_codes[i_code[i]]=true;
     } 
     
-	return true;
+    return true;
 }
 
 
 string Scheme::full_str() {
-	string s = "";
-	string all_p = "";
+    string s = "";
+    string all_p = "";
 
-	for (int i : patterns) {
-		all_p = all_p + code_tab_ptr->patterns[i] + "\n";
-	}
-	s = "[ELB samples = " + to_string(samples) + " patterns = " + to_string(patterns.size()) + "]\n" + all_p;
-	return s;
+    for (int i : patterns) {
+        all_p = all_p + code_tab_ptr->patterns[i] + "\n";
+    }
+    s = "[ELB samples = " + to_string(samples) + " patterns = " + to_string(patterns.size()) + "]\n" + all_p;
+    return s;
 }
 
 
